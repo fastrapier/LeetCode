9336f24b9e4fff9fd471a85d9691b7edf684a56e
fastrapier <fastrapier1@yandex.ru>
2024-03-07 17:40:20 +0300

    Time: 129 ms (66.83%), Space: 16.5 MB (73.01%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.py
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.py
@@ -1,10 +1,5 @@
 class Solution:
-    def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # nums1 - int arr
-        # nums2 - int arr
-        
-        # Вернуть количество индексов элементы которых находятся хотя бы один раз в другом массиве
-        
+    def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:        
         answer = [0, 0]
         
         for n in nums1:
